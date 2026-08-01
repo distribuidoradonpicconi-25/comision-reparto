292b54b02fbace44b5e8dae82e4a0839b25590c1
distribuidoradonpicconi-25 <distribuidoradonpicconi@gmail.com>
2026-08-01 14:40:19 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "comision-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(resp => {
+            return resp || fetch(e.request);
+        })
+    );
+});
